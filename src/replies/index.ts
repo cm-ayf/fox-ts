4b3f4dd8a8abbd5eb62c8181592733da89aa9537
CallMe AsYouFeel <75649254+cm-ayf@users.noreply.github.com>
2022-03-09 23:28:56 +0900

    ignore messages from other guild

--- src/replies/index.ts
+++ src/replies/index.ts
@@ -1,7 +1,11 @@
 import { Message } from 'discord.js';
+import { env } from '../utils';
 import manager from './manager';
 
+const { GUILD_ID } = env;
+
 export async function messageCreate(message: Message) {
+  if (message.guildId !== GUILD_ID) return;
   if (message.author.bot) return;
   await Promise.all(
     manager
